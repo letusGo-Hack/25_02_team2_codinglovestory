e4a19208a0209b87ed6d0899dec3d0de12f2e292
sangsun <hiddenparts@gmail.com>
2025-07-19 13:59:35 +0900

    project add

---
+++ devcodingstory/devcodingstory/devcodingstoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  devcodingstoryApp.swift
+//  devcodingstory
+//
+//  Created by 상선 on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct devcodingstoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
