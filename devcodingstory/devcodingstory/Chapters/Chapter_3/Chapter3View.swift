7b21d852b4af1b41871df030d76588ead4191eb2
seoyounghan <happyunbd364@naver.com>
2025-07-19 15:17:10 +0900

    뷰 추가

---
+++ devcodingstory/devcodingstory/Chapters/Chapter_3/Chapter3View.swift
@@ -0,0 +1,18 @@
+//
+//  Chapter3View.swift
+//  devcodingstory
+//
+//  Created by hanseoyoung on 7/19/25.
+//
+
+import SwiftUI
+
+struct Chapter3View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chapter3View()
+}
